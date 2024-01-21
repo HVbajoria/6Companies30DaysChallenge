da15ea0cff2b79d7e5ee5a16dd783daa03c7842d
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2024-01-21 12:28:26 +0530

    Added solution to Assign Cookies

---
+++ Atlassian/Assign Cookies/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int contentChildren = 0;
+
+        if (g.length == 0 || s.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = 0, si = 0;
+        while (gi < g.length && si < s.length) {
+            if (s[si] >= g[gi]) {
+                contentChildren++;
+                gi++;
+            }
+            si++;
+        }
+
+        return contentChildren;
+    }
+}
